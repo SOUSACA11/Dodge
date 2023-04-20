579e3f4b1e3ccb6a764f0d75ef70b3610188b0ff
JINNY <sousaca05@gmail.com>
2023-04-20 12:44:38 +0900

    230420
    닷지 총알 생성

---
+++ Dodge/Assets/Bullet.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f; // 탄알 이동 속력
+    private Rigidbody bulletRigidbody; // 이동에 사용할 리지드바디 컴포넌트
+
+    void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+        // 게임 오브젝트에서 리지드바디 컴포넌트를 찾아 bulletRigidbody에 할당
+
+        bulletRigidbody.velocity = transform.forward * speed;
+        // 리지드바디 속도 = 앞쪽 방향 * 이동 속력
+
+        Destroy(gameObject, 3f);
+        // 3초 뒤에 자신의 게임 오브젝트 파괴
+    }
+
+    void Update()
+    {
+        
+    }
+}
