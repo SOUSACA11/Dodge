6b204a56bfbac509bc36ef3d84494c903318af2d
JINNY <sousaca05@gmail.com>
2023-04-27 12:38:38 +0900

    230427
    닷지 Ui 스크립트 부분

--- Dodge/Assets/01.Scripts/GameManager.cs
+++ Dodge/Assets/01.Scripts/GameManager.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // UI 관련 라이브러리
+using UnityEngine.SceneManagement; // 신 관리 관련 라이브러리
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject gameoverText; // 게임오버 시 활성화할 텍스트 게임 오브젝트
+    public Text timeText; // 생존 시간을 표시할 텍스트 컴포넌트
+    public Text rexordText; // 최고 기록을 표시할 테스트 컴포넌트
+
+    private float surviveTime; // 생존시간
+    private bool isGameover; //게임오버 상태
     void Start()
     {
-        
+        surviveTime = 0;
+        isGameover = false;
+        // 생존 시간과 게임오버 상태 초기화
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (!isGameover) // 게임오버가 아닌 동안
+        {
+            surviveTime += Time.deltaTime; // 생존 시간 갱신
+            timeText.text = "Time :" + (int)surviveTime; // 갱신한 생존 시간을 timeText 텍스트 컴포넌트를 이용해 표시-
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.R)) // 게임오버 상태에서 R키를 누른 경우
+            {
+                SceneManager.LoadScene("SampleScene"); // SampleScene 씬을 로드
+            }
+        }
+    }
+
+    public void EndGame() // 현재 게임을 게임오버 상태롤 변경하는 메서드
+    {
+        isGameover = true; // 현재 상태를 게임오버 상태로 전환
+        gameoverText.SetActive(true); // 게임오버 텍스트 게임 오브젝트를 활성화
     }
 }
